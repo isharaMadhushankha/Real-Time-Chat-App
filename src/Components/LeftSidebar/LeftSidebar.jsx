f9325c2d480fc1f4e4f9a9de58b3c6be345dc3a7
isharaMadhushankha <isharamadushanka520@gmail.com>
2025-10-04 16:39:45 +0530

    started to develop  the chat box

---
+++ src/Components/LeftSidebar/LeftSidebar.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import "./LeftSidebar.css"
+import assets from '../../assets/assets'
+
+
+const LeftSidebar = () => {
+  return (
+    <div className='ls'>
+        <div className='ls-top'>
+            <div className='ls-nav'>
+                <img src={assets.logo} className="logo" alt="" />
+                <div className='menu'>
+                    <img src={assets.menu_icon} alt="" />
+
+                </div>
+            </div>
+            <div className="ls-search">
+                <img src={assets.search_icon} alt="" />
+                <input type="text" placeholder='Search hear..' />
+            </div>
+        </div>
+        <div className='ls-list'>
+            { Array(10).fill("").map((item,index)=>(
+                <div key={index} className="friend">
+                <img src={assets.profile_img} alt="" />
+                <div>
+                    <p>Ishara Madushanka</p>
+                    <span>hello,How are you?</span>
+
+                </div>
+            </div>
+            ))}
+            
+
+        </div>
+
+    </div>
+  )
+}
+
+export default LeftSidebar
--- src/pages/Chat/Chat.jsx
+++ src/pages/Chat/Chat.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import "./Chat.css";
+import LeftSidebar from '../../Components/LeftSidebar/LeftSidebar';
+import ChatBox from '../../Components/ChatBox/ChatBox';
+import RightSidebar from '../../Components/RightSidebar/RightSidebar';
 
 
 const Chat = () => {
   return (
-    <div> we are on chat page</div>
+    <div className='chat'> 
+      <div className="chat-container">
+        <LeftSidebar/>
+        <ChatBox/>
+        <RightSidebar/>
+      </div>
+  
+    </div>
   )
 }
 
