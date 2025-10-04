0df4c654a86799bc9e50b0511c299ac788cb3d07
isharaMadhushankha <isharamadushanka520@gmail.com>
2025-10-05 00:24:59 +0530

    Start the profileUpdate development

--- src/pages/ProfileUpdate/ProfileUpdate.jsx
+++ src/pages/ProfileUpdate/ProfileUpdate.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
 import "./ProfileUpdate.css"
+import assets from '../../assets/assets'
 
 const ProfileUpdate = () => {
   return (
-    <div> we are on ProfileUpdate</div>
+    <div className='profile'> 
+      <div className="profile-container">
+        <form action="">
+          <h3>Profile details</h3>
+          <label htmlFor='file' id='avatar'  accept ='.png, .jpg, jpeg' hidden>
+          <img src={assets.avatar_icon} alt="" /> 
+          upload file image
+          </label>
+        </form>
+      </div>
+    </div>
   )
 }
 
