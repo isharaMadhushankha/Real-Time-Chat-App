442b0ce329c7b6d3076fc164718bcc1449250c94
isharaMadhushankha <isharamadushanka520@gmail.com>
2025-10-04 23:44:34 +0530

    complate the RightSideBar

--- src/Components/ChatBox/ChatBox.jsx
+++ src/Components/ChatBox/ChatBox.jsx
@@ -1,10 +1,61 @@
-import React from 'react'
-import "./ChatBox.css"
+import React from "react";
+import "./ChatBox.css";
+import assets from "../../assets/assets";
 
 const ChatBox = () => {
   return (
-    <div>ChatBox</div>
-  )
-}
+    <div className="chat-box">
+      <div className="chat-user">
+        <img src={assets.profile_img} alt="" />
+        <p>
+          ishara Madushanka{" "}
+          <img className="dot" src={assets.green_dot} alt="" />
+        </p>
+        <img src={assets.help_icon} alt="" />
+      </div>
+    
+      <div className="chat-msg">
+        <div className="s-msg">
+          <p className="msg">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio
+          </p>
+          <div>
+            <img src={assets.profile_img} alt="" />
+            <p>2.00pm</p>
+          </div>
+        </div>
 
-export default ChatBox
+        <div className="s-msg">
+          <img className="msg-img" src={assets.pic1} alt="" />
+          <div>
+            <img  src={assets.profile_img} alt="" />
+            <p>2.00pm</p>
+          </div>
+        </div>
+
+         <div className="r-msg">
+          <p className="msg">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio
+          </p>
+          <div>
+            <img src={assets.profile_img} alt="" />
+            <p>2.00pm</p>
+          </div>
+        </div>
+      </div>
+
+    
+
+      <div className="chat-input">
+        <input type="text" placeholder="Send a message" />
+        <input type="file" id="image" accept="image/png image/jpeg" hidden />
+        <label htmlFor="image">
+          <img src={assets.gallery_icon} alt="" />
+        </label>
+        <img src={assets.send_button} alt="" />
+      </div>
+    </div>
+  );
+};
+
+export default ChatBox;
--- src/Components/RightSidebar/RightSidebar.jsx
+++ src/Components/RightSidebar/RightSidebar.jsx
@@ -1,11 +1,33 @@
-import React from 'react'
-import "./RightSidebar.css"
-
+import React from "react";
+import "./RightSidebar.css";
+import assets from "../../assets/assets";
 
 const RightSidebar = () => {
   return (
-    <div>RightSidebar</div>
-  )
-}
+    <div className="rs">
+      <div className="rs-profile">
+        <img src={assets.profile_img} alt="" />
+        <h3>
+          ishara Madushanka
+          <img src={assets.green_dot} className="dot" />{" "}
+        </h3>
+        <p>Hey,There i am ishara Madushanka using chat app</p>
+      </div>
+      <hr />
+      <div className="rs-media">
+        <p>Media</p>
+        <div>
+          <img src={assets.pic1} alt="" />
+          <img src={assets.pic2} alt="" />
+          <img src={assets.pic3} alt="" />
+          <img src={assets.pic4} alt="" />
+          <img src={assets.pic3} alt="" />
+          <img src={assets.pic1} alt="" />
+        </div>
+      </div>
+      <button>Logout</button>
+    </div>
+  );
+};
 
-export default RightSidebar
+export default RightSidebar;
